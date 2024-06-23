64bce80d362da0d43193e1a08d5dbe86d0fba99e
apilsrm <sharmaapil05@gmail.com>
2024-06-23 23:58:19 +0545

    text-added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,8 +6,10 @@ const Todo = () => {
         <img src={todo_icon} alt=""  className="w-8"/>
         <h1 className="text-3xl font-semibold justify-self-center">ToDO List</h1>
       </div>
-     <div className="flex"></div>
+     <div className="flex items-center my-7 bg-gray-200 rounded-full"></div>
 
+<input type="text" placeholder="You task here" className="bg-transparent border-0 outline-none flex-1 h-14 pl-6  placeholder:text-slate-500" />
+    <button  className="border-none">ADD + </button>
     </div>
   )
 }
